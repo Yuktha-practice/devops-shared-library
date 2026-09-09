19b6be78f9f5e328d9c4ba6888e12c3de4990880
yuktha <yukthamuki@gmail.com>
2026-09-09 20:38:56 +0530

    jenkinsfile

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -6,6 +6,12 @@ def call(Map config = [:]) {
 
         stages {
 
+            stage('Clean Workspace') {
+                steps {
+                    cleanWs()
+                }
+            }
+
             stage('Checkout') {
                 steps {
                     checkout scm
@@ -14,19 +20,88 @@ def call(Map config = [:]) {
 
             stage('Build') {
                 steps {
-                    echo "Building ${config.application ?: 'application'}"
+                    script {
+
+                        if (config.type == 'node') {
+
+                            sh 'npm ci'
+                            sh 'npm run build'
+
+                        }
+                        else if (config.type == 'java') {
+
+                            sh './mvnw clean package'
+
+                        }
+                        else if (config.type == 'database') {
+
+                            sh 'docker compose config'
+
+                        }
+                    }
                 }
             }
 
             stage('Test') {
                 steps {
-                    echo "Running tests for ${config.application ?: 'application'}"
+                    script {
+
+                        if (config.type == 'node') {
+
+                            sh 'npm test'
+
+                        }
+                        else if (config.type == 'java') {
+
+                            echo "Java tests already executed during Maven package"
+
+                        }
+                        else if (config.type == 'database') {
+
+                            echo "No application tests for database"
+
+                        }
+                    }
                 }
             }
 
             stage('Docker Build') {
                 steps {
-                    echo "Building Docker image for ${config.application ?: 'application'}"
+                    script {
+					
+                        sh "docker build -t ${config.application}:latest ."
+
+                    }
+                }
+            }
+
+            stage('Trivy Scan') {
+                steps {
+                    script {
+					
+                        sh "trivy image ${config.application}:latest"
+                    }
+                }
+            }
+
+            stage('Docker Push') {
+                steps {
+                    script {
+
+						def ecrRegistry = "YOUR_ACCOUNT_ID.dkr.ecr.${config.awsRegion}.amazonaws.com"
+
+						sh """
+							aws ecr get-login-password --region ${config.awsRegion} | \
+							docker login --username AWS --password-stdin ${ecrRegistry}
+
+							docker tag ${config.application}:latest \
+							${ecrRegistry}/${config.ecrRepository}:latest
+
+							docker push \
+							${ecrRegistry}/${config.ecrRepository}:latest
+						"""
+    
+                    }
                 }
             }
         }
