42bbf6c5624c739f79d61be204db462ce07f7344
Installer <cangr2021@gmail.com>
2026-09-17 19:21:06 +0530

    Add Quality Gate stage to CI pipeline

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -99,7 +99,13 @@ def call(Map config = [:]) {
                 }
             }
 			
-			
+			stage('Quality Gate') {
+			    steps {
+			        timeout(time: 5, unit: 'MINUTES') {
+			            waitForQualityGate abortPipeline: true
+			        }
+			    }
+			}
 
             stage('Docker Build') {
                 steps {
